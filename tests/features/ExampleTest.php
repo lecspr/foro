7c5eea60424a5df16528c532a31c69cb585b9cd0
Lore <lorecs@gmail.com>
2017-05-14 16:50:08 -0500

    Fix Tests

--- tests/features/ExampleTest.php
+++ tests/features/ExampleTest.php
@@ -1,21 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+
+class ExampleTest extends FeatureTestCase
 {
-    use DatabaseTransactions;
+    
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    function test_basic_example()
     {
 
-        
+
         $user = factory(App\User::class)->create(['name'=>'Lore', 'email' => 'admin@styde.net']);
 
         $this->actingAs($user,'api')
