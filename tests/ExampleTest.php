8a786c3711fdeda01df9f1e61860fac8a80077ea
Lore <lorecs@gmail.com>
2017-05-14 16:35:19 -0500

    test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,6 +14,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
+
+        $this->assertTrue(false);
         $user = factory(App\User::class)->create(['name'=>'Lore', 'email' => 'admin@styde.net']);
 
         $this->actingAs($user,'api')
